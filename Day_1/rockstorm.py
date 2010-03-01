796d82b0e370d1b2a80907009552a09cad765caf
megatron <megatron@bb8c0089-5e80-0410-bc7d-860d2517b124>
2010-03-01 23:16:53 +0000

    Added copyright notice.

--- Day_1/rockstorm.py
+++ Day_1/rockstorm.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
-# Rockstorm - Asteroids clone written for the 
-# HACMan March Madness Challenge
-# by Jim MacArthur on 1st March 2010
+# Rockstorm - Asteroids clone written for the HACMan March Madness Challenge
+# Copyright (C) 2010 Jim MacArthur. http://www.srimech.com
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # System and pygame modules
 import sys, pygame
