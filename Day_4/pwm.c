65b8bd8642e9d497ddd0a57def3c1033b9abafcb
megatron <megatron@bb8c0089-5e80-0410-bc7d-860d2517b124>
2010-03-04 23:46:19 +0000

    March Madness - Day 4 - RGB Sphere
    Code to make attiny2313 cycle an RGB LED through the colour spectrum using hardware PWM

---
+++ Day_4/pwm.c
@@ -0,0 +1,59 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define bit_get(p,m) ((p) & (m))
+#define bit_set(p,m) ((p) |= (m))
+#define bit_clear(p,m) ((p) &= ~(m))
+#define bit_flip(p,m) ((p) ^= (m))
+#define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m))
+#define BIT(x) (0x01 << (x))
+#define LONGBIT(x) ((unsigned long)0x00000001 << (x)) 
+#define RED OCR0A
+#define GREEN OCR1A
+#define BLUE OCR1B
+#define SPEED 5
+
+int main(void)
+{
+
+	DDRB = BIT(2) | BIT(3) | BIT(4);
+
+	TCNT0 = 0;
+	OCR0A  = 255;
+	TCCR0A = BIT(COM0A1) | BIT(WGM00) | BIT(WGM01);
+	TCCR0B = BIT(CS01);	
+
+	TCNT1  = 0;
+	OCR1A  = 255;
+	OCR1B  = 255;
+	TCCR1A = BIT(COM1A1) | BIT(COM1B1) | BIT(WGM10) | BIT(WGM12);
+	TCCR1B = BIT(CS11);
+
+
+	while(1)
+	{
+		int x = 0;
+		for (x=0; x<255; x++) 
+		{
+			BLUE = 255-x;
+			RED = x;
+			_delay_ms(SPEED);
+		}
+		for (x=0; x<255; x++) 
+		{
+			GREEN = 255-x;
+			BLUE = x;
+			_delay_ms(SPEED);
+		}
+		for (x=0; x<255; x++) 
+		{
+			RED = 255-x;
+			GREEN = x;
+			_delay_ms(SPEED);
+		}		
+
+
+	}
+}
