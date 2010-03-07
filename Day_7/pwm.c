455763e0d3f702734f6418934e0cf6f069e27642
megatron <megatron@bb8c0089-5e80-0410-bc7d-860d2517b124>
2010-03-07 17:36:24 +0000

    RGB Sphere with added serial control.

---
+++ Day_7/pwm.c
@@ -0,0 +1,117 @@
+#define F_CPU 8000000UL
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define bit_get(p,m) ((p) & (m))
+#define bit_set(p,m) ((p) |= (m))
+#define bit_clear(p,m) ((p) &= ~(m))
+#define bit_flip(p,m) ((p) ^= (m))
+#define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m))
+#define BIT(x) (0x01 << (x))
+#define LONGBIT(x) ((unsigned long)0x00000001 << (x)) 
+#define RED OCR0A
+#define GREEN OCR1AL
+#define BLUE OCR1BL
+#define SPEED 5
+
+uint8_t cmdpos = 0;
+uint8_t newr = 0;
+uint8_t newg = 0;
+uint8_t newb = 0;
+
+void pwm_init (void)
+{
+	TCNT0 = 0;
+	OCR0A  = 0;
+	TCCR0A = BIT(COM0A1) | BIT(WGM00) | BIT(WGM01);
+	TCCR0B = BIT(CS01);	
+
+	TCNT1  = 0;
+	OCR1A  = 0;
+	OCR1B  = 0;
+	TCCR1A = BIT(COM1A1) | BIT(COM1B1) | BIT(WGM10) | BIT(WGM12);
+	TCCR1B = BIT(CS11);
+}
+
+void uart_init (void)
+{
+	UCSRB |= (1 << RXEN) | (1 << TXEN); 
+
+	UBRRL = BAUD_PRESCALE; 
+	UBRRH = (BAUD_PRESCALE >> 8);
+
+	UCSRB |= (1 << RXCIE); 
+	sei(); 
+}
+
+void uart_tx (unsigned char tx)
+{
+	while ((UCSRA & (1 << UDRE)) == 0) {};	
+	UDR = tx;
+}
+
+// converts an ascii hex character to decimal
+uint8_t hex2dec (unsigned char hex)
+{
+	if (hex < 57) return hex - 48; // 0-9
+	else if (hex < 70) return hex - 55; //A-F
+	else return hex - 87; // a-f
+}
+
+int main(void)
+{
+	uart_init();
+	DDRB = BIT(2) | BIT(3) | BIT(4);
+	pwm_init();
+	while(1)
+	{
+		_delay_ms(100);
+	}
+}
+
+ISR(USART_RX_vect) 
+{ 
+	char rx; 
+	rx = UDR;
+	uart_tx(rx);
+
+	switch(cmdpos)
+	{
+		case 0:
+			newr = hex2dec(rx)<<4;
+			break;			
+		case 1:
+			newr += hex2dec(rx);
+			break;
+		case 2:
+			newg = hex2dec(rx)<<4;
+			break;			
+		case 3:
+			newg += hex2dec(rx);
+			break;
+		case 4:
+			newb = hex2dec(rx)<<4;
+			break;			
+		case 5:
+			newb += hex2dec(rx);
+			break;
+	}
+	cmdpos++;
+	if (rx == '#') cmdpos = 0;
+	if (rx == '\r')
+	{
+		RED = newr;
+		GREEN = newg;
+		BLUE = newb;
+		cmdpos = 0;
+		uart_tx('\n');
+		uart_tx('O');
+		uart_tx('K');
+		uart_tx('\r');
+		uart_tx('\n');
+	}
+}
---
+++ Day_7/python/colours.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import pygtk, gtk, sys, serial
+
+def dec2hex(n):
+	"""return the hexadecimal string representation of integer n"""
+	n = int(n)
+	if n == 0:
+		return "00"
+	return "%0.2X" % n
+
+class ColourSel:
+	def __init__(self, ser):
+		self.ser = ser
+
+		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		window.set_title("Colour changer")
+		window.set_resizable(False)
+		window.connect("delete_event", self.destroywindow)
+
+		self.colourchooser = gtk.ColorSelection()
+		self.colourchooser.connect("color-changed", self.changecolour)		
+
+		window.add(self.colourchooser)
+
+		self.colourchooser.show()
+		window.show()
+
+	def destroywindow(self, widget, event):
+		gtk.main_quit()
+
+	def changecolour(self, widget, data=None):
+		colour = widget.get_current_color()
+		string = "#"	
+		string = string + dec2hex(int( (float(colour.red) / 65535)*255))
+		string = string + dec2hex(int( (float(colour.green) / 65535)*255))
+		string = string + dec2hex(int( (float(colour.blue) / 65535)*255))
+		self.ser.write(string + "\r")
+
+def main():
+	gtk.main()
+	return 0
+
+if __name__ == "__main__":
+	ser = serial.Serial("/dev/rfcomm0", 9600)
+	ColourSel(ser)
+	main()
