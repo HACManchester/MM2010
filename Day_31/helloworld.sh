db7e2f5522d60019b851bb849b45d8ff63b344ee
megatron <megatron@bb8c0089-5e80-0410-bc7d-860d2517b124>
2010-03-31 21:30:10 +0000

    Add comments for separation.

--- Day_31/helloworld.sh
+++ Day_31/helloworld.sh
@@ -1,11 +1,18 @@
+# Bash
 echo 'Hello from Bash'
+# Python
 echo 'print "Hello from Python"' | python
+# PHP
 php -r 'echo "Hello from PHP\n";' 2>/dev/null
+# C
 echo '#include <stdio.h>' > hello.c
 echo 'int main() { printf("Hello from c\n"); return 0;}' >> hello.c
 gcc -o hello hello.c && ./hello
+# Ruby
 echo 'print "Hello from Ruby\n"' | ruby
 perl -e 'print "Hello from Perl\n"'
+# C++
 echo '#include <iostream>' > hello.cpp
 echo 'int main() { std::cout << "Hello from C++\n"; return 0;}' >> hello.cpp
 g++ -o hello hello.cpp && ./hello
+
